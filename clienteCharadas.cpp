f8e528dc79c552f5c9397dd7e64b69b3b92c01d1
gekicchi <oyarzunm18@gmail.com>
2025-06-26 18:09:43 -0400

    lo q avanze anoche

---
+++ clienteCharadas.cpp
@@ -0,0 +1,98 @@
+// ClienteCharadas.cpp
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+//realizado por Matias Oyarzun y Matias Peters
+#define PUERTO 8001
+#define BUFFERSIZE 1024
+
+using namespace std;
+
+class Cliente 
+{
+    int sockCliente;
+    sockaddr_in confServidor;
+    string nombre;
+
+public:
+    Cliente(const string& nombreCliente) : nombre(nombreCliente) {}
+
+    void conectar() 
+    {
+        crearSocket();
+        configurarServidor();
+        cout << "*****Conectado al servidor****[" << nombre << "]" << endl;
+    }
+
+    void interactuar() 
+    {
+        char buffer[BUFFERSIZE] = {0};
+
+        // Enviar nombre como primer mensaje
+        send(sockCliente, nombre.c_str(), nombre.length(), 0);
+        leerMensaje();
+
+        while (true) 
+        {
+            cout << "[Cliente] ";
+            string entrada;
+            getline(cin, entrada);
+
+            entrada += "\n";
+            send(sockCliente, entrada.c_str(), entrada.length(), 0);
+
+            leerMensaje();
+            if (entrada == "BYE\n") 
+                break;
+        }
+
+        close(sockCliente);
+    }
+
+private:
+    void crearSocket() {
+        if ((sockCliente = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+        {
+            perror("Error creando socket");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    void configurarServidor() 
+    {
+        confServidor.sin_family = AF_INET;
+        confServidor.sin_port = htons(PUERTO);
+        confServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+        if (connect(sockCliente, (struct sockaddr*)&confServidor, sizeof(confServidor)) < 0) 
+        {
+            perror("Error en la    conexión (configurar servidor)");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    void leerMensaje() 
+    {
+        char buffer[BUFFERSIZE] = {0};
+        int valread = read(sockCliente, buffer, BUFFERSIZE);
+        if (valread > 0)
+            cout << "[Servidor] " << buffer << flush << endl;
+    }
+};
+int main() 
+{
+	string nombreCliente;
+	cout << "Ingrese su nombre: ";
+	getline(cin, nombreCliente);
+
+	Cliente cliente(nombreCliente);
+	cliente.conectar();
+	cliente.interactuar();
+
+	return 0;
+}	
---
+++ serverCharadas.cpp
@@ -0,0 +1,174 @@
+// ServidorLab4.cpp
+#include <iostream>
+#include <cstdlib>
+#include <thread>
+#include <vector>
+#include <cstring>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+
+//realizado por Matias Oyarzun y Matias Peters
+#define PUERTO 8001
+#define BUFFERSIZE 1024
+#define WORDQUANTITY 3
+
+using namespace std;
+
+// ------------------------ Clase SesionCliente ------------------------
+class SesionCliente 
+{
+    int sockCliente;
+    sockaddr_in confCliente;
+    string posiblesPalabras[WORDQUANTITY] = {"frutilla", "gato", "mochila"};
+    string pistas[WORDQUANTITY] = {"pista 1", "pista 2", "pista 3"};
+
+public:
+    SesionCliente(int sock, sockaddr_in cliente) : sockCliente(sock), confCliente(cliente) {}
+
+    void atender() 
+    {
+        char buffer[BUFFERSIZE] = {0};
+        char nombre[BUFFERSIZE] = {0};
+        int primerMensaje = 1;
+
+        int random = rand() % WORDQUANTITY;
+        string palabraSecreta = posiblesPalabras[random];
+        string pista = pistas[random];
+
+        while (true) 
+        {
+            memset(buffer, 0, BUFFERSIZE);
+            int valread = read(sockCliente, buffer, BUFFERSIZE);
+            if (valread <= 0) break;
+
+            if (primerMensaje) 
+            {
+                strcpy(nombre, buffer);
+                string saludo = "Hola " + string(nombre) + "\nJugaremos a las Charadas" + "\nIntenta adivinar la palabra secreta";
+                send(sockCliente, saludo.c_str(), saludo.length(), 0);
+                cout << saludo << endl;
+                primerMensaje = 0;
+            } 
+            else 
+            {
+                if (comprobarPalabra(string(buffer),palabraSecreta)) 
+                {
+                    string mensajeFinal = "Correcto!";
+                    send(sockCliente, mensajeFinal.c_str(), mensajeFinal.length(), 0);
+                    cout << mensajeFinal << endl;
+                    break;
+                }
+
+                cout << buffer;
+                string respuesta = "Incorrecto, vuelve a intentarlo";
+
+                send(sockCliente, respuesta.c_str(), respuesta.length(), 0);
+            }
+        }
+
+        close(sockCliente);
+    }
+
+private:
+    string invertirPalabra(const string& palabra)
+    {
+        return string(palabra.rbegin(), palabra.rend());
+    }
+
+    bool comprobarPalabra(const string& palabra, const string& correcta)
+    {
+        cout << palabra << " " << correcta;
+        return palabra.compare(correcta) == 0;
+    }
+};
+
+// ------------------------ Clase Servidor ------------------------
+class Servidor {
+    int sockServidor;
+    sockaddr_in confServidor;
+    vector<thread> clientes;
+
+public:
+    void iniciar(int cantidadClientes) 
+    {
+        crearSocket();
+        configurar();
+        escuchar(cantidadClientes);
+
+        for (int i = 0; i < cantidadClientes; ++i) 
+            aceptarCliente();
+
+        for (auto& cliente : clientes) 
+            if (cliente.joinable()) cliente.join();
+
+        close(sockServidor);
+    }
+
+private:
+    void crearSocket() {
+        if ((sockServidor = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+        {
+            perror("Error creando socket");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    void configurar() {
+        confServidor.sin_family = AF_INET;
+        confServidor.sin_addr.s_addr = htonl(INADDR_ANY);
+        confServidor.sin_port = htons(PUERTO);
+
+        if (bind(sockServidor, (struct sockaddr*)&confServidor, sizeof(confServidor)) < 0) 
+        {
+            perror("Error en bind");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    void escuchar(int n) {
+        if (listen(sockServidor, n) < 0) 
+        {
+            perror("Error en listen");
+            exit(EXIT_FAILURE);
+        }
+        cout << "Servidor escuchando en el puerto " << PUERTO << "..." << endl;
+    }
+
+    void aceptarCliente() {
+        sockaddr_in confCliente;
+        socklen_t tamCliente = sizeof(confCliente);
+        int sockCliente = accept(sockServidor, (struct sockaddr*)&confCliente, &tamCliente);
+        if (sockCliente < 0) 
+        {
+            perror("Error en accept");
+            exit(EXIT_FAILURE);
+        }
+
+        // Crear hebra para atender al cliente
+        clientes.emplace_back(&SesionCliente::atender, SesionCliente(sockCliente, confCliente));
+    }
+};
+
+// ------------------------ main() ------------------------
+int main(int argc, char* argv[]) 
+{
+    if (argc < 2) 
+    {
+        cerr << "Uso: " << argv[0] << " <cantidad_clientes>" << endl;
+        return 1;
+    }
+
+    int cantidadClientes = atoi(argv[1]);
+    if (cantidadClientes <= 0) 
+    {
+        cerr << "Número inválido de clientes" << endl;
+        return 1;
+    }
+
+    Servidor servidor;
+    servidor.iniciar(cantidadClientes);
+
+    return 0;
+}
